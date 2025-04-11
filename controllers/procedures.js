5bac5ae1561208fad193a29f6741268b3593c4b3
Li-8023 <lihe5@asu.edu>
2025-04-11 15:34:29 -0400

    add ngrok

--- controllers/procedures.js
+++ controllers/procedures.js
@@ -32,9 +32,90 @@ exports.addAirplane = async (req, res) => {
 
     res.json({ message: "Airplane added successfully", result });
   } catch (err) {
-    console.error(err);
-    res
-      .status(500)
-      .json({ error: "Failed to add airplane", details: err.message });
+    const sqlMessage = err.sqlMessage || err.message;
+    res.status(400).json({
+      error: "Failed to add airport",
+      details: sqlMessage,
+    });
+  }
+};
+
+//add_airport
+exports.addAirport = async (req, res) => {
+  try {
+    const { airportID, airport_name, city, state, country, locationID } =
+      req.body;
+
+    const [result] = await db.query("CALL add_airport(?, ?, ?, ?, ?, ?)", [
+      airportID,
+      airport_name,
+      city,
+      state,
+      country,
+      locationID,
+    ]);
+
+    res.json({ message: "Airport added successfully", result });
+  } catch (err) {
+    const sqlMessage = err.sqlMessage || err.message;
+    res.status(400).json({
+      error: "Failed to add airport",
+      details: sqlMessage,
+    });
+  }
+};
+
+//add_person
+exports.addPerson = async (req, res) => {
+  try {
+    const {
+      personID,
+      first_name,
+      last_name,
+      locationID,
+      taxID,
+      experience,
+      miles,
+      funds,
+    } = req.body;
+
+    const [result] = await db.query("CALL add_person(?, ?, ?, ?, ?, ?, ?, ?)", [
+      personID,
+      first_name,
+      last_name,
+      locationID,
+      taxID,
+      experience,
+      miles,
+      funds,
+    ]);
+
+    res.status(200).json({ message: "Person added successfully", result });
+  } catch (err) {
+    const sqlMessage = err.sqlMessage || err.message;
+    res.status(400).json({
+      error: "Failed to add person",
+      details: sqlMessage,
+    });
+  }
+};
+
+//grant_or_revoke_pilot_license 
+exports.grantOrRevokePilotLicense = async (req, res) => {
+  try {
+    const { personID, license } = req.body;
+
+    const [result] = await db.query(
+      "CALL grant_or_revoke_pilot_license(?, ?)",
+      [personID, license]
+    );
+
+    res.status(200).json({ message: "License updated successfully", result });
+  } catch (err) {
+    const sqlMessage = err.sqlMessage || err.message;
+    res.status(400).json({
+      error: "Failed to update license",
+      details: sqlMessage,
+    });
   }
 };
--- routes/procedures.js
+++ routes/procedures.js
@@ -3,5 +3,10 @@ const router = express.Router();
 const proceduresController = require("../controllers/procedures");
 
 router.post("/add_airplane", proceduresController.addAirplane);
+router.post("/add_airport", proceduresController.addAirport);
+router.post("/add_person", proceduresController.addPerson);
+router.post("/grant_or_revoke_pilot_license", proceduresController.grantOrRevokePilotLicense);
+
+
 
 module.exports = router;
